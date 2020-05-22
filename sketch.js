3b78ea6f031cd8f92e368b66d0629ecb97ff1dbf
dvdherron <dvdherron@gmail.com>
2020-05-22 12:53:33 +0200

    remove canvas color

--- sketch.js
+++ sketch.js
@@ -9,9 +9,8 @@ function setup() {
   song.playMode("restart");
   amp = new p5.Amplitude();
 
-  // sliderVolume = createSlider(0, 1, 0.5, 0.01);
-  sliderRate = createSlider(0, 2, 1, 0.01);
-  // sliderPan = createSlider(0, 1, 0.5, 0.01);
+  sliderVolume = createSlider(0, 1, 0.5, 0.01);
+  // sliderRate = createSlider(0, 2, 1, 0.01);
 }
 
 function togglePlaying() {
@@ -32,23 +31,10 @@ function loaded() {
 }
 
 function draw() {
-  background(200);
-
   let vol = amp.getLevel();
-  // let diam = map(vol, 0, 0.3, 10, 200);
-  // let bird = document.getElementById("bird");
-  // bird.style.fontWeight = vol * 6000;
   document.documentElement.style.setProperty("--YTLC", vol * 9000);
   document.documentElement.style.setProperty("--YTAS", vol * 9000);
   document.documentElement.style.setProperty("--GRAD", vol * 100);
 
-  // document.documentElement.style.setProperty("--color", "green");
-
-  // fill(
-  //   sliderRate.value() * 50,
-  //   sliderRate.value() * 100,
-  //   sliderRate.value() * 100
-  // );
-  // ellipse(width / 2, height / 2, diam * 5, diam * 5);
-  song.rate(sliderRate.value());
+  song.setVolume(sliderVolume.value());
 }
