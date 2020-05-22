fb3f1ae837ebd3cea7c1a9db4104e592525cb428
dvdherron <dvdherron@gmail.com>
2020-05-22 13:47:43 +0200

    add github link

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-var song;
-var sliderVolume;
-var sliderRate;
-var sliderPan;
+let song;
+let sliderVolume;
+let sliderRate;
+let sliderPan;
 let playing = false;
 
 function setup() {
@@ -10,7 +10,6 @@ function setup() {
   amp = new p5.Amplitude();
 
   sliderVolume = createSlider(0, 1, 0.5, 0.01);
-  // sliderRate = createSlider(0, 2, 1, 0.01);
 }
 
 function togglePlaying() {
@@ -32,6 +31,9 @@ function loaded() {
 
 function draw() {
   let vol = amp.getLevel();
+
+  // get font property values from the :root custom variables
+
   document.documentElement.style.setProperty("--YTLC", vol * 9000);
   document.documentElement.style.setProperty("--YTAS", vol * 9000);
   document.documentElement.style.setProperty("--GRAD", vol * 100);
