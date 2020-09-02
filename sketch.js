f328490cafc65dc94a514d72861d2460b9705dd5
dvdherron <dvdherron@gmail.com>
2020-09-02 13:52:23 +0200

    remove volume slider

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 let song;
-let sliderVolume;
 let sliderRate;
 let sliderPan;
 let playing = false;
@@ -8,8 +7,6 @@ function setup() {
   song = loadSound("assets/Koltrast.mp3");
   song.playMode("restart");
   amp = new p5.Amplitude();
-
-  sliderVolume = createSlider(0, 1, 0.5, 0.01);
 }
 
 function togglePlaying() {
@@ -35,6 +32,4 @@ function draw() {
   document.documentElement.style.setProperty("--YTLC", vol * 9000);
   document.documentElement.style.setProperty("--YTAS", vol * 9000);
   document.documentElement.style.setProperty("--GRAD", vol * 100);
-
-  song.setVolume(sliderVolume.value());
 }
